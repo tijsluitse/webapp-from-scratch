7817490947b1ad82fa5839716ab13086732978da
Wesleyvd <wesley.v.d@hotmail.nl>
2016-02-03 15:01:47 +0100

    Opdracht 5

--- Week 1/opdracht 05/static/js/app.js
+++ Week 1/opdracht 05/static/js/app.js
@@ -1,24 +1,34 @@
-(function(){
+(function() {
 	'use strict';
 	
 	var app = {
 		init: function() {
-
+            routes.init();
 		}
 	};
 
 	var routes = {
 		init: function() {
-
+            window.addEventListener('hashchange', function(tag){
+                var hashSplit = tag.newURL.split('#')[1];
+                sections.toggle(hashSplit);
+            }, false);
 		}
 	};
 
 	var sections = {
-		toggle: function() {
-
+		toggle: function(route) {
+            console.log(route);
+            if (route === 'home') {
+                document.getElementById('bestPractices').style.display = 'none';
+                document.getElementById('home').style.display = 'flex';
+            } else if (route === 'bestPractices') {
+                document.getElementById('bestPractices').style.display = 'flex';
+                document.getElementById('home').style.display = 'none';
+            }
 		}
 	};
 
 	app.init();
 
-})();
+}());
