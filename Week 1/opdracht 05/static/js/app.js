84ffc11ffba23de479435077e5010ecfcb86671d
Wesleyvd <wesley.v.d@hotmail.nl>
2016-02-03 12:14:36 +0100

    Opdracht 4 - opnieuw beginnen | Opdracht 5 - begin van de app.js
    Opdracht 4 vernieuwen, dus from scratch

---
+++ Week 1/opdracht 05/static/js/app.js
@@ -0,0 +1,23 @@
+(function(){
+	'use strict';
+	var app = {
+		init: function() {
+			
+		}
+	};
+
+	var routes = {
+		init: function() {
+
+		}
+	};
+
+	var sections = {
+		toggle: function() {
+
+		}
+	};
+
+	app.init();
+	
+})();
