d87ab082d1001e5d8e0e1fea0298c27f8dad46da
Wesleyvd <wesley.v.d@hotmail.nl>
2016-02-08 13:35:55 +0100

    Week 1 excercise
    Deleted de CSS styling of the sections in JS, using classList

--- Week 1/opdracht 05/static/js/app.js
+++ Week 1/opdracht 05/static/js/app.js
@@ -1,29 +1,40 @@
+/*
+    Credits:    https://github.com/kasszz/web-app-from-scratch
+                https://github.com/beemstb002/WebAppFromScratch
+*/
+
 (function() {
+    // Add strict mode - With strict mode, you can not, for example, use undeclared variables.
 	'use strict';
 	
+    // App object 
 	var app = {
+        // init method in app that calls the init method in the routes object
 		init: function() {
             routes.init();
 		}
 	};
 
+    // Routes object
 	var routes = {
 		init: function() {
             window.addEventListener('hashchange', function(tag){
+                // call the toggle method in the sections object
                 sections.toggle(tag.newURL.split('#')[1]);
             }, false);
 		}
 	};
 
+    // Sections object
 	var sections = {
 		toggle: function(route) {
             console.log(route);
             if (route === 'home') {
-                document.getElementById('bestPractices').style.display = 'none';
-                document.getElementById('home').style.display = 'flex';
+                document.getElementById('bestPractices').classList.remove('active');
+                document.getElementById('home').classList.add('active');
             } else if (route === 'bestPractices') {
-                document.getElementById('bestPractices').style.display = 'flex';
-                document.getElementById('home').style.display = 'none';
+                document.getElementById('bestPractices').classList.add('active');
+                document.getElementById('home').classList.remove('active');
             }
 		}
 	};
